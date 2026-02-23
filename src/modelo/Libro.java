b8eb76ca405212c0a1d183f28447d9adb7555526
Estefanía <estefaniacarrillosuarez@gmail.com>
2026-02-23 21:51:06 +0000

    añadido comentario y mejoras en Libro

--- src/modelo/Libro.java
+++ src/modelo/Libro.java
@@ -1,17 +1,27 @@
 package modelo;
 
-public class Libro {
+// Representa un libro dentro del sistema de biblioteca
+// Contiene la información necesaria y lógica para gestionar la disponibilidad, reservas y préstamos
 
+public class Libro {
+    // Identificador único
     private String isbn;
+    // Datos del libro
     private String titulo;
     private String autor;
+    // Información editorial sobre el libro
     private int anioPublicacion;
     private String editorial;
+    // Género del libro (enum)
     private Genero genero;
+    // El número total de copias en la biblioteca
     private int copiasTotales;
+    // Copias disponibles actualmente para préstamo
     private int copiasDisponibles;
+    // Estado del libro
     private EstadoLibro estado;
-
+    
+    // Contructor que asigna los valores iniciales del libro
     public Libro(String isbn, String titulo, String autor, int anioPublicacion, String editorial, Genero genero, int copiasTotales) {
         
         this.isbn = isbn;
@@ -26,6 +36,7 @@ public class Libro {
 
     }
 
+    // Permiten acceder a la información del libro sin cambiarla
     public String getIsbn() {
         return isbn;
     }
@@ -50,10 +61,13 @@ public class Libro {
     public int getCopiasDisponibles() {
         return copiasDisponibles;
     }
-
+    // Comprueba si hay copias disponibles para préstamo
     public boolean hayCopiasDisponibles() {
         return copiasDisponibles > 0;
     }
+    // Realiza el préstamo de una copia del libro 
+    // Reduce las copias disponibles
+    // Si se agotan, el estado del libro pasa a prestado
     public void prestar() {
         if (copiasDisponibles <=0) {
             throw new IllegalArgumentException("No hay copias disponibles");
@@ -65,7 +79,8 @@ public class Libro {
             estado = EstadoLibro.PRESTADO;
         }
     }
-
+    // Registra la devolución de una copia
+    // Aumenta las copias disponibles y cambia el estado
     public void devolver() {
         if (copiasDisponibles < copiasTotales) {
             copiasDisponibles++;
